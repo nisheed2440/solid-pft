aed44c1c2d6e762131a24084fc219d5d92d90cad
Nisheed Jagadish <nisheed2016@gmail.com>
2022-02-11 11:46:45 +0530

    Added TS build for generated components

---
+++ packages/pft-assets/src/types/index.d.ts
@@ -0,0 +1,1 @@
+export type SVGPops = Partial<SVGSVGElement & HTMLElement>;
--- packages/pft-assets/tools/convert-assets.ts
+++ packages/pft-assets/tools/convert-assets.ts
@@ -1,15 +1,16 @@
 import * as fs from 'fs-extra';
 import * as path from 'path';
+import del from 'del';
 import fg from 'fast-glob';
 import { optimize, loadConfig, OptimizeOptions } from 'svgo';
 import { paramCase, pascalCase } from 'change-case';
 
 const PARTS = ['accessories', 'body', 'face', 'head', 'pose', 'facial-hair'];
-const TEST_PARTS = ['test'];
+// const TEST_PARTS = ['test'];
 let svgoConfig: OptimizeOptions = {};
 
 const getPeepsAssetPaths = async (part: string): Promise<string[]> => {
-    const peepsAssets = await fg(`src/${part}/*.svg`);
+    const peepsAssets = await fg(`src/assets/peeps/${part}/*.svg`);
     return peepsAssets;
 }
 
@@ -19,14 +20,17 @@ const optimizeSvg = async (filePath: string): Promise<string> => {
     return (optimizedSvg as any).data;
 }
 
-const createSvgComponent = (svg: string): string => {
+const addPropsToSvg = (svg: string): string => {
+    let componentTpl = 'import { Component } from "solid-js";\n';
     const svgWithProps = svg.replace(/(?<=<svg.*?)(>)/i, " {...props}>");
-    return `export default (props = {}) => (${svgWithProps});\n`;
+    componentTpl += `\nconst SolidComponent: Component<{}> = (props = {}) => (${svgWithProps});\n`;
+    componentTpl += 'export default SolidComponent;\n';
+    return componentTpl;
 }
 
 const saveSvgComponent = async (part: string, fileName: string, svg: string): Promise<void> => {
     const compFileName = paramCase(fileName);
-    const filePath = path.join(__dirname, `../lib/components/${part}/${compFileName}.tsx`);
+    const filePath = path.join(__dirname, `../build/components/${part}/${compFileName}.tsx`);
     await fs.ensureDir(path.dirname(filePath));
     await fs.writeFile(filePath, svg, {encoding: 'utf8'});
 }
@@ -34,31 +38,33 @@ const saveSvgComponent = async (part: string, fileName: string, svg: string): Pr
 const createSvgIndex = async (part: string, filePaths: string[]): Promise<void> => {
     let indexTpl = 'import { lazy } from "solid-js";\n';
     indexTpl += '\nexport default {\n';
-    filePaths.forEach((filePath) => {
+    for (const filePath of filePaths) {
         const fileName = path.basename(filePath, '.svg');
         const compName = pascalCase(fileName);
         const compFileName = paramCase(fileName);
         indexTpl += `  ${compName}: lazy(() => import("./${compFileName}")),\n`;
-    });
+    };
     indexTpl += '}\n';
-    const filePath = path.join(__dirname, `../lib/components/${part}/index.ts`);
+    const filePath = path.join(__dirname, `../build/components/${part}/index.ts`);
     await fs.ensureDir(path.dirname(filePath));
     await fs.writeFile(filePath, indexTpl, {encoding: 'utf8'});
 }
 
+const createSvgComponents = async (part: string, filePaths: string[]): Promise<void> => {
+    for (const filePath of filePaths) {
+        const fileName = path.basename(filePath, '.svg');
+        const svg = await optimizeSvg(filePath);
+        const svgWithProps = addPropsToSvg(svg);
+        await saveSvgComponent(part, fileName, svgWithProps);
+    }
+}
+
 (async () => {
+    await del(['build', 'lib']);
     svgoConfig = await loadConfig();
-    // PARTS.forEach(async (part) => {
-    //     console.log(await getPeepsAssetPaths(part));
-    // });
     PARTS.forEach(async (part) => {
         const filePaths = await getPeepsAssetPaths(part);
-        filePaths.forEach(async (filePath) => {
-            const fileName = path.basename(filePath, '.svg');
-            const optimizedSVG = await optimizeSvg(filePath);
-            const svgComponent = createSvgComponent(optimizedSVG);
-            await saveSvgComponent(part, fileName, svgComponent);
-        });
+        await createSvgComponents(part, filePaths);
         await createSvgIndex(part, filePaths);
     });
 })();
