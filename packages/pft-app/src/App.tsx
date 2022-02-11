4f76c2fe678f2d292257a001bf79c75ba0a21991
Nisheed Jagadish <nisheed2016@gmail.com>
2022-02-12 01:44:10 +0530

    Fixed issue with yarn workspaces

--- packages/pft-app/src/App.tsx
+++ packages/pft-app/src/App.tsx
@@ -1,4 +1,5 @@
 import type { Component } from 'solid-js';
+import { poseSittingComponents } from '@solid-pft/core';
 
 import logo from './logo.svg';
 import styles from './App.module.css';
@@ -20,6 +21,7 @@ const App: Component = () => {
           Learn Solid
         </a>
       </header>
+      <poseSittingComponents.Bike />
     </div>
   );
 };
--- packages/pft-core/tools/convert-assets.ts
+++ packages/pft-core/tools/convert-assets.ts
@@ -6,7 +6,7 @@ import { getPeepsAssetPaths, createRootIndex, createSvgComponents, createSvgInde
     const svgoConfig = await loadConfig();
     PEEPS_PARTS.forEach(async (part) => {
         const filePaths = await getPeepsAssetPaths(part);
-        await createSvgComponents(part, filePaths, svgoConfig);
+        await createSvgComponents(part, filePaths, svgoConfig || undefined);
         await createSvgIndex(part, filePaths);
     });
     await createRootIndex(PEEPS_PARTS);
